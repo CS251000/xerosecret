8cd9f4878c816745e5a85acb771acf1773bf5ef3
chirag <chiragsinghal04@gmail.com>
2024-08-18 22:10:12 +0530

    -f

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <p className="hidden">The answer is "iamtheanswer"</p>
+        <p className="hidden">The answer is &quot;iamtheanswer&quot;</p>
         {children}
         </body>
     </html>
